c5ecbfd8a7193516d5d0594d82c9a86153b7a6d4
Teshome <teshomebf@gmail.com>
2026-02-18 10:55:31 +0300

    Optimize shuffle algorithm using Fisher-Yates (function #49)

--- 001-day1/day1-50functions.js
+++ 001-day1/day1-50functions.js
@@ -3,48 +3,48 @@
           // 1 - 10 FUNCTIONS
 /*======================================*/
 
-// return a number
+// 1
 function getNumber() {
     return 42;
 }
-
+// 2
 function getString() {
     return "hello";
 }
 
 
-// - return a boolean true
+// 3
 function getBoolean() {
     return true;
 }
-// - return an array
+// 4
 function giveArray() {
     return ["Javor", "Alex", "Albert"];
 }
-// - return an object
+// 5
 function createObject() {
     return {
         coder: "Javor",
         study: () => "Mastering Functions in JavaScript"
     };
 }
-// - return null
+// 6
 function getNothing() {
     return null;
 }
-// - return undefined
+// 7
 function getUndefinedValue() {
     
 }
-// - return a function
+// 8
 function createFunction() {
     return () => "I'm returned function";
 }
-// - return your age
+// 9
 function getAge() {
     return 30;
 }
-// - return your name
+// 10
 function getName() {
     return "Javor";
 }
@@ -54,25 +54,25 @@ function getName() {
           // 11 - 20 FUNCTIONS
 /*======================================*/
 
-// return today's Date
+// 11
 function dateToday() {
     return new Date();
 }
 console.log(dateToday());
-// return random number between max and min
+// 12
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max -min + 1)) + min;
 }
 console.log(randomNumber(20, 10));
-// - return a boolean false
+// 13
 function getBoolean() {
     return false;
 }
-// - return numbers array
+// 14
 function getNumbersArray() {
     return [1, 2, 3, 4, 5];
 }
-// - return an object
+// 15
 function createObject() {
     return {
         coder: "Javor",
@@ -83,26 +83,26 @@ function createObject() {
 }
 const dev = createObject();
 console.log(`${dev.coder} is ${dev.age} years old ${dev.heIsDoing()}`);
-// - return null
+// 16
 function getNothing() {
     return null;
 }
 console.log(getNothing());
-// - return undefined
+// 17
 function giveUndefinedValue(name) {
 }
 console.log(giveUndefinedValue("Javor"));
-// - return a function
+// 18
 function createFunction() {
     return () => "Doesn't just console.log";
 }
 let getFunction = createFunction();
 console.log(getFunction());
-// - return empty array
+// 19
 function createEmptyArray() {
     return [];
 }
-// - return object with methods
+// 20
 function greetGuest(greeting) {
     return {
         greet: (name) => `${greeting}, ${name}! Welcome to our function drill session.`
@@ -118,31 +118,31 @@ function greetGuest(greeting) {
           // 21 - 30 FUNCTIONS
 /*======================================*/
 
-// returns a string
+// 21
 function getHello() {
     return "hello";
 }
-//returns a reversed string
+// 22
 function reverseString(str) {
     return str.split("").reverse().join("");
 } 
-// returns today's date as a string
+// 23
 function getCurrentDay() {
     return `${new Date()}`;
 }
-// returns the longer of two strings
+// 24
 function getLongerString(str1, str2) {
     return (str1.length > str2.length) ? str1 : str2; 
 }
-// returns a random boolean value
+// 25
 function getRandomBoolean() {
     return (Math.floor(Math.random() * 2)) ? true : false;
 }
-// returns even or odd based on the input number 
+// 26
 function checkEvenOrOdd(num) {
     return (num % 2 === 0) ? "Even" : "Odd";
 }
-// returns an array of 5 random numbers between max and min, with a minimum difference of 5 between them
+// 27
 function getFiveRandomNumbers(min, max) {
 
      if((max - min) < 5) {
@@ -155,7 +155,7 @@ function getFiveRandomNumbers(min, max) {
 
     return arr;
 }
-// returns an object with the name and current time
+// 28
 function createNameAndTimeObject(name) {
 
     return {
@@ -163,11 +163,11 @@ function createNameAndTimeObject(name) {
         currentTime: new Date().toLocaleTimeString()
     }
 }
-// returns a function that takes a number and returns the number multiplied by 2
+// 29
 function getDoubleFunction() {
     return (x) => x * 2; 
 }
-// returns the first element of an array
+// 30
 function getFirstElement(arr) {
     return arr[0];
 }
@@ -177,43 +177,44 @@ function getFirstElement(arr) {
           // 31 - 40 FUNCTIONS
 /*======================================*/
 
+// 31
 function getSquare(num) {
     return num * num;
 }
-
+// 32
 function getAverage(num1, num2) {
     return (num1 + num2) / 2;
 }
-
+// 33
 function sum(arr) {
     return arr.reduce((acc, curr) => {
         return acc += curr;
     }, 0);
 }
-
+// 34
 function turnToUpperCase(str) {
     return str.toUpperCase();
 }
-
+// 35
 function getVowels(str) {
     let vowels = "aeiou";
     let vowelsInStr = vowels.split("").filter(v => str.toLowerCase().includes(v) ); 
 
     return vowelsInStr.length;
 }
-
+// 36
 function getRandomElement(arr) {
     let index = Math.floor(Math.random() * arr.length);
 
     return arr[index];
 }
-
+// 37
 function checkPositiveOrNegativeOrZero(num) {
     if(num > 0) return "Positive"
     if(num < 0) return "Negative"
     if(num === 0) return "Zero"
 }
-
+// 38
 function getLongestString(arr){
     let longestString = arr[0];
 
@@ -229,7 +230,7 @@ function getLongestString(arr){
     };
 
 }
-
+// 39
 function createPersonObject(name, age) {
     return {
         _name: name,
@@ -238,7 +239,7 @@ function createPersonObject(name, age) {
           return `Hello, I'm ${this._name} and I'm ${this._age} years old`}
     }
 }
-
+// 40
 function getAddFunction(num) {
     return (x) => num + x; 
 }
@@ -248,7 +249,7 @@ function getAddFunction(num) {
           // 41 - 50 FUNCTIONS
 /*======================================*/
 
-
+// 41
 function getLongestWord(sentence) {
     let arr = sentence.split(" ");
     let longestWord = arr[0];
@@ -261,7 +262,7 @@ function getLongestWord(sentence) {
 
     return longestWord;
 }
-
+// 42
 function createArrayOfEvenNumbers(num) {
     let arr = [];
     for(let i = 0; i <= num; i++) {
@@ -269,7 +270,7 @@ function createArrayOfEvenNumbers(num) {
     }
   return arr;
 }
-// 
+// 43
 function removeDuplicatesFromArray(arr) {
     let uniqueArr = [];
     
@@ -281,8 +282,8 @@ function removeDuplicatesFromArray(arr) {
 
     return uniqueArr;
     }
-
-    function createLetterCountObject(str) {
+// 44
+function createLetterCountObject(str) {
         return str
         .split("")
         .reduce((acc, curr) => {
@@ -291,8 +292,8 @@ function removeDuplicatesFromArray(arr) {
             return acc;
         }, {});
     }
-
-    function checkCommonElements(arr1, arr2) {
+// 45
+function checkCommonElements(arr1, arr2) {
         if(arr1.length === 0 || arr2.length === 0) return "Arrays shouldn't be empty";
 
         for(let i = 0; i < arr2.length; i++) {
@@ -303,8 +304,8 @@ function removeDuplicatesFromArray(arr) {
         return false;
     }
 
-
-    function getFactorial(num) {
+// 46
+function getFactorial(num) {
         let product = 1;
         for(let i = 1; i <= num; i++) {
             product *= i;
@@ -313,15 +314,15 @@ function removeDuplicatesFromArray(arr) {
         return product;
     }
 
-
-    function getSecondLargestNumber(numbers) {
+// 47
+function getSecondLargestNumber(numbers) {
         const sortedNumber = [...numbers].sort((a, b) => a - b); 
 
         return sortedNumber[sortedNumber.length - 2];
     }
 
-
-    function getEveryOtherLetterCapitalized(str) {
+// 48
+function getEveryOtherLetterCapitalized(str) {
         
         return str.split("").map((el, index) => {
             if(index % 2 !== 0) {
@@ -330,24 +331,19 @@ function removeDuplicatesFromArray(arr) {
             return el.toLowerCase();
         }).join("");
     }
-
-
-
-    function getRandomOrderArrayElement(arr) {
+// 49
+function getRandomOrderArrayElements(arr) {
         
-        let newArr = [];
-        while(newArr < arr.length) {
-            let index = Math.floor(Math.random() * arr.length);
-            if(!newArr.includes(arr[index])) {
-                newArr.push(arr[index]);
-            }
-            
-        }
+        let newArr = [...arr];
+        for(let i = newArr.length -1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
 
+            [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+        }
         return newArr;
     }
-
-    function getFunctionRunningOriginalFunctionNumberTimes(fn, times) {
+// 50
+function getFunctionRunningOriginalFunctionNumberTimes(fn, times) {
         // I'm beaten by this challenge
         return () => {
             let results = [];
