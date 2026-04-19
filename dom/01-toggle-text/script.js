05459e2686d133afe173c31458b5c85befe8b589
Teshome <teshomebf@gmail.com>
2026-04-19 11:19:35 +0300

    feat(dom): enhance UI with interaction feedback, refined shadows and improved visual hierarchy of 01-toggle-text project

--- dom/01-toggle-text/script.js
+++ dom/01-toggle-text/script.js
@@ -6,17 +6,25 @@ const button = document.querySelector(".container__btn-toggle");
 let isUpdated = false;
 
 function toggleText() {
-  isUpdated = !isUpdated;
+  title.computedStyleMap.opacity = "0.5";
 
-  if(isUpdated) {
+  setTimeout(() => {
+    if(isUpdated) {
     title.textContent = "Hello, World!";
     title.classList.remove("active");
     button.textContent = "Update greeting";
+
+    isUpdated = false;
   } else {
       title.textContent = "I have started the journey of mastering JavaScript.";
       title.classList.add("active");
       button.textContent = "Reset";
+
+      isUpdated = true;
   }
+
+  title.computedStyleMap.opacity = "1";
+  }, 250);
 }
 
 button.addEventListener("click", toggleText);
