80238efb3b99a660e12a779d32b2acc797c1f139
Teshome <teshomebf@gmail.com>
2026-04-25 07:06:51 +0300

    feat(counter): add reset action, improve interaction feedback, and refine UI structure and consistency

--- dom/03-counter/script.js
+++ dom/03-counter/script.js
@@ -4,6 +4,8 @@ const counterBtnContainer = document.querySelector(".container__counter-btn");
 const valueContainer = document.querySelector(".container__current-value");
 const decreaseBtn = document.querySelector(".container__btn-dec");
 
+const reset = document.querySelector(".container__btn-reset");
+
 let currentValue = 0;
 
 
@@ -18,11 +20,15 @@ updateUI();
 
 counterBtnContainer.addEventListener("click", (e) => {
   const clickedBtn = e.target;
+  const action = clickedBtn.dataset.action;
 
-  if(clickedBtn.classList.contains("container__btn-inc")) {
+  if(!action) return;
+  
+
+  if(action === "increment") {
     currentValue++;
 
-  } else if(clickedBtn.classList.contains("container__btn-dec") && currentValue > 0){
+  } else if(action === "decrement" && currentValue > 0){
     currentValue--;
    
   }
@@ -30,3 +36,9 @@ counterBtnContainer.addEventListener("click", (e) => {
    updateUI();
 
 });
+
+reset.addEventListener("click", (e) => {
+  if(e.target.dataset.action === "reset") currentValue = 0;
+
+  updateUI();
+})
