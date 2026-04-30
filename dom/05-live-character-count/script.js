95513609cf45e9a453367d735f02587e7ec7212d
Teshome <teshomebf@gmail.com>
2026-04-30 14:30:24 +0300

    feat(character-counter): enable real feedback submission and improve user trust communication

--- dom/05-live-character-count/script.js
+++ dom/05-live-character-count/script.js
@@ -18,8 +18,10 @@ function updateUI() {
   const isNearLimit = remaining <= 10 && remaining > 0;
 
   feedback.classList.remove("feedback--warning", "feedback--limit");
+  countDisplay.classList.remove("max--limit");
 
   if(isOverLimit) {
+    countDisplay.classList.add("max--limit");
     feedback.classList.add("feedback--limit");
   } else if(isNearLimit) {
     feedback.classList.add("feedback--warning");
@@ -39,7 +41,29 @@ function updateUI() {
     // countDisplay.style.color = color;
 }
 
+
 updateUI();
 
 textarea.addEventListener("input", updateUI);
 
+const form = document.querySelector("form")
+
+form.addEventListener("submit", (e) => {
+  const text = textarea.value;
+  const count = text.length;
+  const isOverLimit = count >= MAX_LIMIT;
+
+  if(isOverLimit) {
+    e.preventDefault();
+
+    feedback.textContent = "Message exceeds allowed limit.";
+    feedback.classList.add("feedback--limit");
+  }
+
+});
+
+
+window.addEventListener("pageshow", () => {
+  form.reset();
+  updateUI();
+})
